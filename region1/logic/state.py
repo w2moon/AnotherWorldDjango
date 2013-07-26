0c618d8821800a51198b84158bb247813a38ee40
w2moon <w2moon@gmail.com>
2013-07-26 18:13:46 +0800

    add battle state

---
+++ region1/logic/state.py
@@ -0,0 +1,7 @@
+'''
+Created on 2013-7-26
+
+@author: pengw
+'''
+def state_normal():
+    pass
---
+++ wl/functask.py
@@ -0,0 +1,41 @@
+'''
+Created on 2013-7-26
+
+@author: pengw
+'''
+
+class functask(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.tasks = []
+        
+    def add(self,func,*args):
+        self.tasks.insert(0,[func,args])
+        
+    def adds(self,calls):
+        self.tasks = calls + self.tasks
+        
+    def addtail(self,func,*args):
+        self.tasks.append([func,args])
+        
+    def remove(self,func):
+        self.tasks.remove(func)
+        
+    def next(self):
+        task = self.tasks[0]
+        del self.tasks[0]
+        dt = None
+        while task != None and dt == None:
+            dt = task[0](*(task[1]))
+            if dt == None:
+                task = self.tasks[0]
+                del self.tasks[0]
+            
+        return dt
