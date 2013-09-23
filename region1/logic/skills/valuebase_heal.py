10e10679f1c6c0125c07eb065805329a43b6b7ef
w2moon <w2moon@gmail.com>
2013-09-23 18:01:12 +0800

    add skill logic

--- region1/logic/skills/valuebase_heal.py
+++ region1/logic/skills/valuebase_heal.py
@@ -3,14 +3,14 @@ Created on 2013-7-29
 
 @author: pengw
 '''
-def do(skill,trigger,event_targets,value):
+def do(skill,trigger,event_targets):
     params = skill.getBase()['param']
     
     targets = skill.getBattleField().select_target(skill.getWarrior().getPlayer(),skill.getWarrior(),params[1],skill.getWarrior().getTraveller().getNature(),True,trigger,event_targets)
     tasks = []
     
     for t in targets:
-        tasks.append([t.incHP,[value*params[2]]])
+        tasks.append([t.incHP,[skill.getWarrior().getPeroperty(params[5])*params[2]]])
         
     tasks.append([skill.delay,[0.4]])
     
